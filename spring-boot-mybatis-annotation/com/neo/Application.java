94b4883085328fac2daccb4510e7dbeac8746e0a
WLyiyi <35355348+WLyiyi@users.noreply.github.com>
2018-01-12 11:40:38 +0800

    Create Application.java

---
+++ spring-boot-mybatis-annotation/com/neo/Application.java
@@ -0,0 +1,16 @@
+# spring-boot-mybatis-mysql
+package com.neo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.neo.mapper")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+//作用：相当于启动一个Tomcat。
